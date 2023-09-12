e860216dd091e1169573ce8b38d9ed9ec7bb7002
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2023-09-12 15:43:43 +0100

    new

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,24 +1,29 @@
-import { logo } from "../../assets";
+import logo from "../../assets/logo.svg";
 
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex w-full mb-10 pt-3 justify-between items-center flex-row">
-        <img src={logo} alt="ifeanyiosi" />
+        <button type="button" className="flex items-center  gap-2">
+          {" "}
+          <img className="w-[48px] " src={logo} alt="ifeanyiosi" />
+          <h1 className="font-bold font-inter text-[20px]">Ifeanyiosi</h1>
+        </button>
 
-        <button type="button" onClick={() => window.open("")}>
+        <button type="button" className="black_btn" onClick={() => window.open("")}>
           {" "}
           Github
         </button>
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />{" "}
+        Paraphrase with <br className="max-md:hidden" />{" "}
         <span className="orange_gradient">A.I</span>
       </h1>
 
       <h2 className="desc">
-        WIth this simple too you can simplify your reading, this app allows you summarize lengthy articles into clear and concise summaries
+        This app lets you condense long articles into clear and concise
+        summaries, which can then be used by an AI for text paraphrasing.
       </h2>
     </header>
   );
